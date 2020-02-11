774fc46d3c5e71f32904895570c6eb849221c70d
Gil Beyruth <firstpixel@gmail.com>
2020-02-10 23:30:09 -0300

    Adjusts on endpoint /category/id, adding exceptions

--- src/main/java/com/firstpixel/services/CategoryService.java
+++ src/main/java/com/firstpixel/services/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.firstpixel.domain.Category;
 import com.firstpixel.repositories.CategoryRepository;
+import com.firstpixel.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService  {
@@ -16,6 +17,7 @@ public class CategoryService  {
 	
 	public Category find(Integer id) { 
 		Optional<Category> obj = repo.findById(id); 
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Object not found! Id: " + id + ", Type: " + Category.class.getName()));
 	}
 }
