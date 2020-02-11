b384cbdf1f830d6fc879dadbc65b664f634ea5f5
Gil Beyruth <firstpixel@gmail.com>
2020-02-10 23:01:37 -0300

    Association product category manytomany

--- src/main/java/com/firstpixel/repositories/ProductRepository.java
+++ src/main/java/com/firstpixel/repositories/ProductRepository.java
@@ -1,16 +1,11 @@
 package com.firstpixel.repositories;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.firstpixel.domain.Category;
+import com.firstpixel.domain.Product;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-	
-	
-	
 }
